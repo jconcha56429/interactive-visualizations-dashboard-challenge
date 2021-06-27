ab58830b44542c697b63f8f9c28429fd7ba28d05
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-06-26 20:55:20 -0700

    created plot function; attempt select query

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,20 +1,21 @@
 function build_plot(samp){
-    test = []
+    samp_vals = []
+    var otu_ids = []
 
     for(i=0;i<10;i++){
-        test.push(samp.sample_values[i]);
-        //var otu_ids = samp.otu_ids[i];
+        samp_vals.push(samp.sample_values[i]);
+        otu_ids.push(`UTO ${samp.otu_ids[i]}`);
         //var otu_labels = samp.otu_labels[i];
         var trace1 = [{
-            x: [1,2,3,4,5,6,7,8,9,10],
-            y: test,
-            type: "bar"
-            
+            x: samp_vals,
+            y: otu_ids,
+            type: "bar",
+            orientation: 'h',
         }]
         Plotly.newPlot("bar",trace1)
     }
-    console.log(test)
-
+    console.log(samp_vals)
+    console.log(otu_ids)
 }
 
 
@@ -26,8 +27,10 @@ function optionChanged(id){d3.json("../StarterCode/samples.json").then(function 
         .attr("value",sample.id)
         .text(sample.id)
     })
+    console.log(id)
     //console.log(id)
     var numero_uno = samples[0]
     build_plot(numero_uno)
+
 })}
 optionChanged()
