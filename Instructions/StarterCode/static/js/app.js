b81786b528c31dd56c5c2619509b033b9b115f87
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-06-28 13:38:42 -0700

    rough draft of bar chart simulator

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -5,7 +5,6 @@ function build_plot(samp){
     for(i=0;i<10;i++){
         samp_vals.push(samp.sample_values[i]);
         otu_ids.push(`UTO ${samp.otu_ids[i]}`);
-        //var otu_labels = samp.otu_labels[i];
         var trace1 = [{
             x: samp_vals,
             y: otu_ids,
@@ -14,8 +13,6 @@ function build_plot(samp){
         }]
         Plotly.newPlot("bar",trace1)
     }
-    console.log(samp_vals)
-    console.log(otu_ids)
 }
 
 
@@ -27,10 +24,9 @@ function optionChanged(id){d3.json("../StarterCode/samples.json").then(function 
         .attr("value",sample.id)
         .text(sample.id)
     })
-    console.log(id)
-    //console.log(id)
+    var filter_samp = samples.filter(samp => samp['id'] == id)
     var numero_uno = samples[0]
     build_plot(numero_uno)
-
+    build_plot(filter_samp[0])
 })}
 optionChanged()
