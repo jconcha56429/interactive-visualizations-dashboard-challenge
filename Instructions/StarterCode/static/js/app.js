634583a750ebdcb80bc80583757ae5b26c2c109f
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-06-28 13:57:18 -0700

    added hover text to bar chart

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,13 +1,19 @@
 function build_plot(samp){
-    samp_vals = []
+    var samp_vals = []
     var otu_ids = []
+    var labels = []
 
     for(i=0;i<10;i++){
         samp_vals.push(samp.sample_values[i]);
         otu_ids.push(`UTO ${samp.otu_ids[i]}`);
+        labels.push(samp.otu_labels[i]);
         var trace1 = [{
             x: samp_vals,
             y: otu_ids,
+            mode: 'markers',
+            marker:{size:1},
+            hoverinfo:"text",
+            text: labels,
             type: "bar",
             orientation: 'h',
         }]
@@ -28,5 +34,6 @@ function optionChanged(id){d3.json("../StarterCode/samples.json").then(function 
     var numero_uno = samples[0]
     build_plot(numero_uno)
     build_plot(filter_samp[0])
+    console.log(numero_uno)
 })}
 optionChanged()
